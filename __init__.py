e4d638be9510ef2cda6db5770d9fab582052b9d6
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-18 10:49:24 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,16 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from miley import DB_URI
+from miley import LOAD, NO_LOAD, LOGGER
 
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+    # This generates a list of modules in this folder for the * in __main__ to work.
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
+                   and f.endswith(".py")
+                   and not f.endswith('__init__.py')]
+
+    if LOAD or NO_LOAD:
+        to_load = LOAD
+        if to_load:
+            if not all(any(mod == module_name for module_name in all_modules) for mod in to_load):
+                LOGGER.error("Invalid loadorder names. Quitting.")
+                quit(1)
+
+        else:
+            to_load = all_modules
+
+        if NO_LOAD:
+            LOGGER.info("Not loading: {}".format(NO_LOAD))
+            return [item for item in to_load if item not in NO_LOAD]
+
+        return to_load
+
+    return all_modules
 
 
-BASE = declarative_base()
-SESSION = start()
+ALL_MODULES = sorted(__list_all_modules())
+LOGGER.info("Modules to load: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
