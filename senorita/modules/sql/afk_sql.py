2d94de59cc9c64235f8abd896b0e0a06b56c533c
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-19 19:00:12 +0530

    Update afk_sql.py

--- senorita/modules/sql/afk_sql.py
+++ senorita/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, Integer
 
-from miley.modules.sql import BASE, SESSION
+from senorita.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
