7d57c685d6bdb50e6c22cc80ed448ed6d9715eab
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-19 19:08:49 +0530

    Update disable_sql.py

--- senorita/modules/sql/disable_sql.py
+++ senorita/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from miley.modules.sql import SESSION, BASE
+from senorita.modules.sql import SESSION, BASE
 
 
 class Disable(BASE):
