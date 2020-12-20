1ba6ef303e65add512f6db4e783d003fd2325e4a
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-20 13:03:03 +0530

    Update lydia_sql.py

--- senorita/modules/sql/lydia_sql.py
+++ senorita/modules/sql/lydia_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from miley.modules.sql import BASE, SESSION
+from senorita.modules.sql import BASE, SESSION
 
 
 class ChatbotChats(BASE):
