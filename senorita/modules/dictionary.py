cb8aefddc70ae513c948ff924a423c3c1eae9c90
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-20 21:56:21 +0530

    Update dictionary.py

--- senorita/modules/dictionary.py
+++ senorita/modules/dictionary.py
@@ -4,7 +4,7 @@ import requests
 from telegram import Bot, Message, Update, ParseMode
 from telegram.ext import CommandHandler, run_async
 
-from miley import dispatcher
+from senorita import dispatcher
 
 
 @run_async
