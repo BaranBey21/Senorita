566a320be9950619a2cbbaf155074f11073c7c35
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-21 14:56:19 +0530

    Update __main__.py

--- senorita/__main__.py
+++ senorita/__main__.py
@@ -42,11 +42,11 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
-🔥 - /start: start the bot
-🔥 - /help: PM's you this message.
-🔥 - /help <module name>: PM's you info about that module.
-🔥 - /source: Information about my source.
-🔥 - /settings:
+❤️ - /start: start the bot
+❤️ - /help: PM's you this message.
+❤️ - /help <module name>: PM's you info about that module.
+❤️ - /source: Information about my source.
+❤️ - /settings:
    🔹 - in PM: will send you your settings for all supported modules.
    🔹 - in a group: will redirect you to pm, with all that chat's settings.
 {}
