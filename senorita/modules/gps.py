97cd9719e3e1f869484ab5f1acaff85fac2d12f2
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-21 14:26:22 +0530

    Update gps.py

--- senorita/modules/gps.py
+++ senorita/modules/gps.py
@@ -10,11 +10,11 @@ from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
                                                                    
-from miley import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
-from miley.__main__ import STATS, USER_INFO
-from miley.modules.disable import DisableAbleCommandHandler
-from miley.modules.helper_funcs.extraction import extract_user
-from miley.modules.helper_funcs.filters import CustomFilters
+from senorita import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from senorita.__main__ import STATS, USER_INFO
+from senorita.modules.disable import DisableAbleCommandHandler
+from senorita.modules.helper_funcs.extraction import extract_user
+from senorita.modules.helper_funcs.filters import CustomFilters
 
 from geopy.geocoders import Nominatim
 from telegram import Location
