a32337bd60ac0f5ae6c740d602d85022ab54035c
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-21 10:30:39 +0530

    Update blsticker_sql.py

--- senorita/modules/sql/blsticker_sql.py
+++ senorita/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from miley.modules.sql import SESSION, BASE
+from senorita.modules.sql import SESSION, BASE
 
 
 class StickersFilters(BASE):
